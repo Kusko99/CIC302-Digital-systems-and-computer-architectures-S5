2447c0d6d7f1e960fe8b92a20a365c6c037f3d31
Kusko99 <fernandaofo120@gmail.com>
2024-03-12 14:14:22 -0300

    Começo de algo

---
+++ simulador_funcoes_booleanas/boolean_fucntion.py
@@ -0,0 +1,7 @@
+class boolean_function:
+    @staticmethod
+    def variables(function: str):
+        pass
+    @staticmethod
+    def char_decider(function: str, char: chr):
+        pass
---
+++ simulador_funcoes_booleanas/main.py
@@ -0,0 +1,9 @@
+print("*******************************")
+print("Simulador de funções booleanas")
+print("*******************************")
+
+print("Digite sua função booleana abaixo:")
+print("OBS: para AND -> *, para OR -> + e para NOT -> '")
+function = input()
+function_list = function.split()
+print(function_list)
